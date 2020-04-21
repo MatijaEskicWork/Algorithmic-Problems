36bb04b08c0b0aed251c6418c4d0f7ec989a6242
Matija Eskic <58684238+MatijaEskicWork@users.noreply.github.com>
2020-04-21 18:45:49 +0200

    Create D-FiveFive(AtCoder).cpp

---
+++ AdHoc/D-FiveFive(AtCoder).cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+ 
+using namespace std;
+ 
+bool isPrime(int x) {
+    int root = (int)sqrt(x);
+    for (int i = 2; i <= root; i++) {
+        if (x % i == 0) return false;
+    }
+    return true;
+}
+ 
+ 
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("2 ");
+    int ans = n - 1;
+    int cnt = 3;
+    while (ans) {
+        if (isPrime(cnt) && cnt % 5 == 1) {
+            printf("%d ", cnt);
+            ans--;
+        }
+        cnt++;
+    }
+    return 0;
+}
+@MatijaEskicWork
